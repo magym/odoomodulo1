a4a4bcce7ab5ecd94a0a6d2784667e58e69a3996
Magym <Magdis_gn_15@hotmail.com>
2018-12-21 15:44:19 -0600

    creación del módulo

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+	'name':'Modulo  odoo 1',
+	'version':'1.1',
+	'author':'Magda GN',
+	'category':'Tools',
+	'summary':'Módulo creado para 1er objetivo',
+	'depends':['base'],
+	'data':[
+	'views/odoomodulo_view.xml'
+	],
+	'installable':True,
+	'auto_install':True,
+}
